3d6193e1de4b8a0da99d28353d0f496dda9a7e89
Asif Iftekher Pial <iftekherpial67@gmail.com>
2023-06-16 17:58:40 +0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'Asif Iftekher Pial', // Your GitHub org/user name. (Required)
+    username: 'Asif-Iftekher-Pial', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
